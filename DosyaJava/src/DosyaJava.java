c7a3b908bd7ea74ad6e74e76dd22de904a824f3a
GokceKy <99399762+GokceKy@users.noreply.github.com>
2022-11-07 23:27:20 +0300

    Update DosyaJava.java

--- DosyaJava/src/DosyaJava.java
+++ DosyaJava/src/DosyaJava.java
@@ -102,31 +102,29 @@ public class DosyaJava implements ActionListener{
        listOfStrings =new ArrayList();
        
          try {
-        // list that holds strings of a file
+        
        
        
-        // load data from file
+        
         BufferedReader bf = new BufferedReader(
             new FileReader("C:\\Users\\Aybala\\Desktop/dosya.txt"));
        
-        // read entire line as string
+       
         String line = bf.readLine();
        
-        // checking for end of file
+        
         while (line != null) {
             listOfStrings.add(line);
             line = bf.readLine();
         }
        
-        // closing bufferreader object
+        
         bf.close();
        
-        // storing the data in arraylist to array
         String[] array
             = listOfStrings.toArray(new String[0]);
        
-        // printing each line of file
-        // which is stored in array
+      
         for (String str : array) {
             System.out.println(str);
         }
